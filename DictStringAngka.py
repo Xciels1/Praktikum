e950bfbd93cedbaf46998f3ce1dd20cd24e561b7
Rico Lanang Ona <146321312+Xciels1@users.noreply.github.com>
2025-03-11 14:26:48 +0700

    Update DictStringAngka.py

--- DictStringAngka.py
+++ DictStringAngka.py
@@ -9,3 +9,15 @@ def angka_ke_kata(angka):
 print(angka_ke_kata(3))  # Output: tiga
 print(angka_ke_kata(7))  # Output: tujuh
 print(angka_ke_kata(10)) # Output: Angka di luar jangkauan
+
+
+def angka_ke_kata2(angka):
+    angka_dict = {
+        0: "nol", 1: "satu", 2: "dua", 3: "tiga", 4: "empat",
+        5: "lima", 6: "enam", 7: "tujuh", 8: "delapan", 9: "sembilan"
+    }
+    kata_list = [angka_dict[int(digit)] for digit in str(angka)]
+    return ' '.join(kata_list)
+
+
+print(angka_ke_kata(3124))
